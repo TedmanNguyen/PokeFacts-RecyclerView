5f73a85c010d66b75b00d459155a2d2268a0af4d
TedmanNguyen <97199766+TedmanNguyen@users.noreply.github.com>
2023-04-07 19:33:19 -0700

    Finished Project 6 for Codepath

---
+++ app/src/main/java/com/codepath/pokemonview/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.codepath.pokemonview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var pokeList: MutableList<Triple<String, String, String>>
+    private lateinit var rvPoke: RecyclerView
+    var pokeImageURL = ""
+    var pokeNameURL = ""
+    var pokeWeightURL = ""
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rvPoke = findViewById(R.id.poke_list)
+        pokeList = mutableListOf()
+        for (i in 0 until 20) {
+            getPokeData()
+        }
+
+        Log.d("Pokemon List", pokeList.toString())
+    }
+
+    private fun getPokeData() {
+        val client = AsyncHttpClient()
+        val poke = Random.nextInt(600)
+        client["https://pokeapi.co/api/v2/pokemon/$poke", object : JsonHttpResponseHandler() {
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Headers,
+                json: JsonHttpResponseHandler.JSON
+            ) {
+                pokeImageURL = json.jsonObject.getJSONObject("sprites").getString("front_default")
+                Log.d("PokeImage", "response successful")
+                Log.d("pokeImageURL", pokeImageURL)
+                pokeNameURL = json.jsonObject.getString("name")
+                Log.d("PokeName", "response successful")
+                Log.d("pokeNameURL", pokeNameURL)
+                pokeWeightURL = json.jsonObject.getString("weight")
+                val weight = "Weight: " + pokeWeightURL
+                Log.d("PokeWeight", "response successful")
+                Log.d("pokeWeightURL", pokeWeightURL)
+
+                pokeList.add(Triple(pokeImageURL, pokeNameURL, weight))
+                Log.d("Poke list", pokeList.toString())
+
+                val adapter = PokeAdapter(pokeList)
+                rvPoke.layoutManager = LinearLayoutManager(this@MainActivity)
+                rvPoke.adapter = PokeAdapter(pokeList)
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                errorResponse: String,
+                throwable: Throwable?
+            ) {
+                Log.d("Poke data error", errorResponse)
+            }
+
+        }]
+    }
+}
---
+++ app/src/main/java/com/codepath/pokemonview/PokeAdapter.kt
@@ -0,0 +1,48 @@
+package com.codepath.pokemonview
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+class PokeAdapter(private val pokeList: MutableList<Triple<String, String, String>>) : RecyclerView.Adapter<PokeAdapter.ViewHolder>() {
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val pokeImage: ImageView
+        val pokeName: TextView
+        val pokeWeight: TextView
+
+        init {
+            pokeImage = view.findViewById(R.id.poke_image)
+            pokeName = view.findViewById(R.id.poke_name)
+            pokeWeight = view.findViewById(R.id.poke_weight)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.poke_item, parent, false)
+
+        return ViewHolder(view)
+    }
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        Glide.with(holder.itemView)
+            .load(pokeList[position].first)
+            .centerCrop()
+            .into(holder.pokeImage)
+
+
+        val pokeNameText = holder.pokeName
+        pokeNameText.text = pokeList[position].second
+
+        val pokeWeightText = holder.pokeWeight
+        pokeWeightText.text = pokeList[position].third
+
+    }
+
+    override fun getItemCount() = pokeList.size
+}
+
+
